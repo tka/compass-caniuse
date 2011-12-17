c4e55af3ee89191f54afa11bb500a11f66349536
tka lu <tkatka@gmail.com>
2011-12-17 15:27:32 +0800

    add css property & caniuse mapping

--- caniuse.rb
+++ caniuse.rb
@@ -26,9 +26,42 @@ caniuse_js_data = open(cache_file).read
 caniuse = ExecJS.eval(caniuse_js_data)
 
 caniuse.each do |key, data|
-  next unless key.match(/^css/)
   stats = data["stats"]
   puts key
   puts "======"
   puts stats
 end
+#  css                => caniuse
+mapping= {
+  "background-clip"   => "background-img-opts",
+  "background-origin" => "background-img-opts",
+  "background-size"   => "background-img-opts",
+  "border-radius"     => "border-radius",
+  "box-orient"        => "flexbox",
+  "box-align"         => "flexbox",
+  "box-flex"          => "flexbox",
+  "box-flex-group"    => "flexbox",
+  "box-ordinal-group" => "flexbox",
+  "box-direction"     => "flexbox",
+  "box-lines"         => "flexbox",
+  "box-pack"          => "flexbox",
+  "box-shadow"        => "css-boxshadow",
+  "box-sizing"        => "css3-boxsizing",
+  "column-count"      => "multicolumn",
+  "column-gap"        => "multicolumn",
+  "column-width"      => "multicolumn",
+  "rule-width"        => "multicolumn",
+  "rule-style"        => "multicolumn",
+  "rule-color"        => "multicolumn",
+  "column-rule"       => "multicolumn",
+  "transition-property"  => "css-transitions",
+  "transition-duration"  => "css-transitions",
+  "transition-timing-function" => "css-transitions",
+  "transition-delay" => "css-transitions",
+  "transition"       => "css-transitions"
+}
+
+# TODO:
+# Compass Images => SVG in CSS backgrounds
+# Compass Transform => CSS3 Transforms 
+# Compass Transform =>  CSS3 3D Transforms 
